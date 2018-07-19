22dedd1122e68f3c855e5c8519f4251d38771d82
willholsten <willholsten@gmail.com>
2018-07-19 06:15:56 +0100

    remove modal component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Route, Switch, withRouter } from "react-router-dom";
 import BurgerMenu from "../components/BurgerMenu/BurgerMenu";
 
-import styled from "styled-components";
-import BookingModal from "../components/BookingModal/BookingModal";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
